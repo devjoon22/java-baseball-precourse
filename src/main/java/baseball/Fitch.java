84cb0606500f33907bb466ff1c6026241f06b598
devjoon22 <devjoon22@gmail.com>
2022-10-03 03:48:20 +0900

    feat(Fitch): count ball and strike method
    Add count ball and strike method in Fitch class

--- src/main/java/baseball/Fitch.java
+++ src/main/java/baseball/Fitch.java
@@ -3,14 +3,15 @@ package baseball;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
 
 public class Fitch {
 
     List<Integer> fitches;
-
 
     public Fitch() {
         fitches = new ArrayList<>();
@@ -30,5 +31,42 @@ public class Fitch {
 
     public List<Integer> getFitches() {
         return fitches;
+    }
+
+    public Map<String, Integer> countBallAndStrike(String inputNumbersString) {
+        List<Integer> inputNumbers = numbersStringToIntegerList(inputNumbersString);
+        Map<String, Integer> fitchResultMap = new HashMap<>();
+        for (int i = 0; i < inputNumbers.size(); i++) {
+            int fitch = inputNumbers.get(i);
+            String fitchResultString = isBallStrikeNothing(fitch, i);
+            Integer fitchCnt = fitchResultMap.getOrDefault(fitchResultString, 0);
+            fitchResultMap.put(fitchResultString, fitchCnt + 1);
+        }
+        return fitchResultMap;
+    }
+
+    private List<Integer> numbersStringToIntegerList(String numbersString) {
+        char[] inputNumbersCharArray = numbersString.toCharArray();
+        List<Integer> integerList = new ArrayList<>();
+        for (char numberChar : inputNumbersCharArray) {
+            integerList.add(Character.getNumericValue(numberChar));
+        }
+
+        return integerList;
+    }
+
+    private String isBallStrikeNothing(int fitch, int index) {
+        if (fitches.contains(fitch)) {
+            return (fitches.indexOf(fitch) == index) ? "strike" : "ball";
+        }
+        return "nothing";
+    }
+
+    public void setTestFitches() {
+        fitches.clear();
+
+        fitches.add(4);
+        fitches.add(1);
+        fitches.add(8);
     }
 }
